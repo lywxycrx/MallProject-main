4d9ec02dd374a7d865de2613775a4d40686877cd
akaNemo <160277612@qq.com>
2025-04-03 18:47:22 +0800

    登陆页面修改

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="outDiv">
-
     <div class="loginDiv">
-      <div class="imgDiv">
-      </div>
+      <div class="imgDiv"></div>
       <el-form 
-      :model="ruleForm" 
-      status-icon 
-      :rules="rules" 
-      ref="ruleForm" 
-      label-width="60px" 
-      class="form">
+        :model="ruleForm" 
+        status-icon 
+        :rules="rules" 
+        ref="ruleForm" 
+        label-width="80px" 
+        class="form">
+        
         <el-form-item label="Account" prop="uid">
           <el-input type="text" v-model="ruleForm.uid" autocomplete="off"></el-input>
         </el-form-item>
@@ -18,190 +17,166 @@
         <el-form-item label="Password" prop="pwd">
           <el-input type="password" v-model="ruleForm.pwd" autocomplete="off"></el-input>
         </el-form-item>
+
         <div class="radioDiv">
           <el-radio v-model="type" label="0">Customer</el-radio>
           <el-radio v-model="type" label="1">Vendor</el-radio>
         </div>
-        <Dialog ref="dialog"></Dialog>
+
+        <Dialog ref="dialog" />
+
         <el-form-item>
-          <el-button  @click="submitForm('ruleForm')">Log in</el-button>
-          <el-button class="regBtn"  @click="register()">Sign up</el-button>
-          <el-button @click="resetForm('ruleForm')">Reset</el-button>
+          <div class="button-group">
+            <el-button @click="submitForm('ruleForm')">Log in</el-button>
+            <el-button @click="register()">Sign up</el-button>
+            <el-button @click="resetForm('ruleForm')">Reset</el-button>
+          </div>
         </el-form-item>
       </el-form>
     </div>
-    
   </div>
 </template>
 
 <script>
 import jwt from 'jwt-decode';
-import {mapMutations} from 'vuex'
-import Dialog from './register'
+import { mapMutations } from 'vuex';
+import Dialog from './register';
 
- export default {
-   components: {
-     Dialog,
-   },
-    data() {
-      var validateUser = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('Please enter your account'));
-        } else {
-          callback();
-        }
-      };
-      var validatePwd = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('Please enter your passwrod'));
-        } else {
-          callback();
-        }
-      };
+export default {
+  components: { Dialog },
+  data() {
+    const validateUser = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please enter your account'));
+      } else {
+        callback();
+      }
+    };
+    const validatePwd = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please enter your password'));
+      } else {
+        callback();
+      }
+    };
 
-      return {
-        type: null,        // 登录人员身份控制
-        ruleForm: {
-          uid: '',
-          pwd: '',
-        },
-        rules: {
-          uid: [
-            { validator: validateUser, trigger: 'blur' }
-          ],
-          pwd: [
-            { validator: validatePwd, trigger: 'blur' }
-          ],
-        }
-      };
-    },
-
-    methods: {
-      register() {
-        this.$refs.dialog.dialogVisible = true;
+    return {
+      type: null,
+      ruleForm: {
+        uid: '',
+        pwd: '',
       },
-
-      ...mapMutations('loginModule', ['setUser']),
-      submitForm(formName) {
-        if(this.type === null) {
-          this.$message({
-            type: 'error',
-            message: 'Please choose your login identity'
-          })
-        }else {
-          this.$refs[formName].validate((valid) => {
-            if (valid) {
-              console.log('校验通过', this.ruleForm)
-              let {uid, pwd} = this.ruleForm;
-              this.$api.getLogin({
-                uid,
-                pwd,
-                type: this.type
-              }).then(res => {
-                console.log('返回数据：', res.data)
-                if(res.data.status === 200){
-                  console.log(jwt(res.data.data))
-                  // 数据存储
-                  let obj = {
-                    uid: jwt(res.data.data).uid,
-                    name: jwt(res.data.data).name,
-                    address: jwt(res.data.data).address,
-                    token: res.data.data,
-                    type: this.type,
-                  }
-                  console.log(obj);
-                  this.setUser(obj)
-                  // 存储至本地
-                  localStorage.setItem('user', JSON.stringify(obj))
-                  if(this.type == 1) {
-                    this.$router.push('/aHome')
-                  }else{
-                    this.$router.push('/home')
-                  }
-                }else{
-                  this.$message({
-                    type: 'error',
-                    message: 'Incorrect account or password entry'
-                  })
-                }
-              })
+      rules: {
+        uid: [{ validator: validateUser, trigger: 'blur' }],
+        pwd: [{ validator: validatePwd, trigger: 'blur' }],
+      },
+    };
+  },
+  methods: {
+    register() {
+      this.$refs.dialog.dialogVisible = true;
+    },
+    ...mapMutations('loginModule', ['setUser']),
+    submitForm(formName) {
+      if (this.type === null) {
+        this.$message({
+          type: 'error',
+          message: 'Please choose your login identity',
+        });
+        return;
+      }
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          const { uid, pwd } = this.ruleForm;
+          this.$api.getLogin({ uid, pwd, type: this.type }).then((res) => {
+            if (res.data.status === 200) {
+              const decoded = jwt(res.data.data);
+              const obj = {
+                uid: decoded.uid,
+                name: decoded.name,
+                address: decoded.address,
+                token: res.data.data,
+                type: this.type,
+              };
+              this.setUser(obj);
+              localStorage.setItem('user', JSON.stringify(obj));
+              this.$router.push(this.type == 1 ? '/aHome' : '/home');
             } else {
-              console.log('error submit!!');
-              return false;
+              this.$message({
+                type: 'error',
+                message: 'Incorrect account or password entry',
+              });
             }
           });
         }
-
-      },
-
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
-    }
-  }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
 <style scoped>
+.outDiv {
+  position: fixed;
+  background-image: url('../assets/demo1.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
 
-  .outDiv {
-    position: fixed;
-    background-image: url('../assets/demo1.jpg');
-    background-size: cover;
-    overflow: hidden;
-    height: 100%;
-    width: 100%;
-    top:0;
-    left: 0;
-  }
+.loginDiv {
+  width: 460px;
+  padding: 60px 40px 30px;
+  margin: 100px auto;
+  background: rgba(255, 255, 255, 0.95);
+  border-radius: 15px;
+  position: relative;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+}
 
-  .loginDiv {
-    width: 470px;
-    height: 320px;
-    margin: 150px auto;
-    border-radius: 15px;
-    border: 1px solid rgb(193, 193, 193);
-   
-  }
+.imgDiv {
+  position: absolute;
+  top: -50px;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 100px;
+  width: 100px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  background-image: url('../assets/logo.png');
+  background-size: cover;
+  background-position: center;
+}
 
-  .imgDiv {
-    position: absolute;
-    height: 100px;
-    width: 100px;
-    left: 46%;
-    background-color: rgb(255, 255, 255);
-    border: 1px solid rgb(193, 193, 193);
-    border-radius: 100%;
-    background-image: url('../assets/logo.png');
-    background-size: cover;
-    top: 17%
-  }
+.form {
+  margin-top: 40px;
+}
 
-  .form {
-    position: absolute;
-    top: 35%;
-    left: 35%;
-    /* transform: translate(-50%, -30%); */
-  }
+.radioDiv {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
 
-  .radioDiv {
-    position: relative;
-    padding-bottom: 7%;
-    left: 37%;
-  }
+.button-group {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .el-input {
-    width: 150% !important;
-  }
+.el-input {
+  width: 100%;
+}
 
-  .el-button {
-    position: absolute;
-    margin-left: 100%;
-  }
-
-  .regBtn {
-    position: absolute !important;
-    left: 47.5%;
-  }
-
-</style>>
-  
+.el-button {
+  min-width: 80px;
+}
+</style>
